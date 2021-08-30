3a8aee411fa433abf1549d956210ed8bd2a6329b
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-08-30 13:56:31 +0300

    create serializers for models: Blog, Comment

---
+++ blog_project/blog/serializers.py
@@ -0,0 +1,12 @@
+from .models import *
+from rest_framework import serializers
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
