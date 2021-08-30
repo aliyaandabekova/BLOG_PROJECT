65b759fe6fa8184a5879a143df0b29baecd68ee7
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-08-30 15:11:41 +0300

    create ViewClass for: get, create blogs

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from .permissions import *
+from .serializers import *
+from .models import *
 
-# Create your views here.
+class BlogViewSet(ViewSet):
+    permission_classes = [BlogPermissions]
+    serializer_class = BlogSerializer
+    def list(self,request):
+        blogs = Blog.objects.all()
+        serializer = self.serializer_class(blogs,many=True)
+        return Response(serializer.data,status=200)
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
