0b1077f1e7d13e6fb89624c3d8fb60ad032625cb
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-08-30 15:10:51 +0300

    create path ('blogs/') for:get, create

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -2,5 +2,9 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
+    path('blogs/',BlogViewSet.as_view({
+        'get':'list',
+        'post':'create'
+    }),name='blogs'),
 
 ]
