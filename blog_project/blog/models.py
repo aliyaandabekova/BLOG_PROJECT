3dd8963ff3635ad2bceec192ee00ae8d4efc99f4
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-08-30 13:55:39 +0300

    create models: Blog, Comment

---
+++ blog_project/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
+    text = models.CharField(max_length=500)
+    date_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    text = models.CharField(max_length=100)
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
