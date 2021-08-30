31cdf9670ecc401489217c7704199c527edc962a
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-08-30 15:09:50 +0300

    create tests for: get blogs, create blog

--- blog_project/blog/tests.py
+++ blog_project/blog/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.contrib.auth.models import User, Group
+from rest_framework.test import APITestCase
+from .models import *
+from django.urls import reverse
 
-# Create your tests here.
+class BlogTesCase(APITestCase):
+    def setUp(self) -> None:
+        self.admin = User.objects.create_user(username='admin',password='1234')
+        self.group = Group.objects.create(name='admin')
+        self.admin.groups.add(self.group)
+        self.blog = Blog.objects.create(title='Math',text='1+1')
+    def test_get_blogs(self):
+        self.url = reverse('blogs')
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code,200)
+    def test_create_blog(self):
+        self.client.login(username='admin',password='1234')
+        self.url = reverse('blogs')
+        data = {'title':'Math','text':'how much?'}
+        self.response = self.client.post(self.url,data)
+        self.assertEqual(self.response.status_code,201)
