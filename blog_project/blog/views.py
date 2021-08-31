38e04f6af99786010150fa1bc73b1f736803566e
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-09-01 01:04:02 +0300

    create View-Class with functions:list(detail of blog with comments), create(create a comment)

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -18,3 +18,22 @@ class BlogViewSet(ViewSet):
             serializer.save()
             return Response(serializer.data,status=201)
         return Response(serializer.errors,status=400)
+
+class CommentViewSet(ViewSet):
+    permission_classes = [CommentPermissions]
+    serializer_class = CommentSerializer
+    def list(self,request,blog_id):
+        blog = Blog.objects.get(id=blog_id)
+        serializer = BlogDetailSerializer(blog)
+        return Response(serializer.data,status=200)
+    def create(self,request,blog_id):
+        user = request.user
+        blog = Blog.objects.get(id=blog_id)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            text = serializer.data.get('text')
+            Comment.objects.create(user=user,text=text,blog=blog)
+            serializer1 = BlogDetailSerializer(blog)
+            return Response(serializer1.data, status=201)
+        return Response(serializer.errors,status=400)
+
