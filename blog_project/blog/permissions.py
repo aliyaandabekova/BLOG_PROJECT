718c8e591447822f92c6874c7f279cfcfb3f2de4
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-09-01 00:59:05 +0300

    create permission-class for Comment-view

--- blog_project/blog/permissions.py
+++ blog_project/blog/permissions.py
@@ -11,3 +11,13 @@ class BlogPermissions(permissions.BasePermission):
             return True
         if group == 'admin' and request.method in ['GET','POST','PUT','DELETE']:
             return True
+
+class CommentPermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        group = None
+        if isinstance(request.user,User):
+            group = request.user.groups.all()[0].name
+        if request.method in SAFE_METHODS:
+            return True
+        if group == 'user' and request.method == 'POST':
+            return True
