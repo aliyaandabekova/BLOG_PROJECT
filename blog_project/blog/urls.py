63c8957884007502bccf803a706ab3a9f20723a6
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-09-01 12:17:32 +0300

    create path for: delete blog, add parameter name for tests

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -6,9 +6,13 @@ urlpatterns = [
         'get':'list',
         'post':'create'
     }),name='blogs'),
+    path('blog/<int:blog_id>/',BlogViewSet.as_view({
+        'get':'retrieve',
+        'delete':'destroy',
+    }),name='delete_blog'),
     path('comments/<int:blog_id>/',CommentViewSet.as_view({
         'get':'list',
         'post':'create',
-    }))
+    }),name='comments')
 
 ]
